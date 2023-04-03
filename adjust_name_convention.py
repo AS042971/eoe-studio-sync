966e16b719fe0857a8165bda648f6c6280de183f
XiaoyuMaxwellWu <53926289+XiaoyuMaxwellWu@users.noreply.github.com>
2023-04-02 19:33:46 -0700

    Update adjust_name_convention.py
    适配windows

--- adjust_name_convention.py
+++ adjust_name_convention.py
@@ -1,15 +1,16 @@
 import os
 import pandas as pd
+import shutil
 
 
 def adjust_csv(database_path: str):
     if not os.path.exists(database_path):
         raise Exception('audio路径不存在')
-    df = pd.read_csv(database_path, encoding="utf-8-sig", header=None)
+    df = pd.read_csv(database_path, encoding="utf-8-sig")
     for i in range(0, len(df)):
         if df.iloc[i - 1, 4] == '全员':
             df.iloc[i - 1, 4] = 'EOE'
-    df.to_csv(database_path, index=False, header=None)
+    df.to_csv(database_path, index=False)
 
 
 def adjust_two_files_name():
@@ -20,6 +21,6 @@ def adjust_two_files_name():
                 if len(split) > 1 and '合唱' in split[1]:
                     split[1] = 'EOE'
                 new_filename = ' '.join(split)
-                os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
+                shutil.copy(os.path.join(directory, filename), os.path.join(directory, new_filename))
     adjust_file_name('./audio')
     adjust_file_name('./cover')
